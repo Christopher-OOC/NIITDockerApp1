cb62713b155a546bb22f489f2dd2da4429e61f32
Christopher-OOC <olojedechristopher24@gmail.com>
2025-02-21 14:18:07 +0100

    commit

--- src/main/java/com/example/niit/DockerApp/DockerAppApplication.java
+++ src/main/java/com/example/niit/DockerApp/DockerAppApplication.java
@@ -1,10 +1,19 @@
 package com.example.niit.DockerApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -15,26 +24,87 @@ public class DockerAppApplication {
 	}
 }
 
-@RestController
+@Controller
 class DockerController {
 
-	@GetMapping(value = "/message")
-	public String test() {
+	Logger LOGGER = LoggerFactory.getLogger(DockerController.class);
 
-		return """
-				Welcome to Docker! You have just
-				started a new journey!
-				""";
+	@GetMapping(value = "/")
+	public String homePage(Model model) throws IOException, ClassNotFoundException {
+		model.addAttribute("message", new Message());
+
+		File file = new File("C:/Users/Christopher-JavaLord/Downloads/DockerApp/src/main/resources/messages.txt");
+		List<Message> messageList = new ArrayList<>();
+
+		if (file.length() == 0) {
+			model.addAttribute("noContent", "No message");
+		}
+		else {
+			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file));
+
+
+			while (inputStream.available() != 0) {
+				System.out.println("Messages: " + inputStream.readObject());
+				messageList.add((Message) inputStream.readObject());
+			}
+		}
+		messageList.forEach(System.out::println);
+
+		model.addAttribute("messages", messageList);
+
+		return "index";
 	}
 
-	@GetMapping(value = "/test")
-	public Map<String, String> jsonOutput() {
+	@PostMapping(value = "/submit")
+	public String postMessage(@ModelAttribute("message") Message message,
+							  Model model,
+							  RedirectAttributes redirectAttributes) throws IOException {
 
-		return Map.of(
-				"name", "Christopher",
-				"faculty", "MMS",
-				"floor", "4th"
-		);
+		redirectAttributes.addFlashAttribute(  "success", "Your message is sent successfully!");
+
+		LOGGER.info("Your message, {}", message);
+		File file = new File("C:/Users/Christopher-JavaLord/Downloads/DockerApp/src/main/resources/messages.txt");
+		ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file));
+		outputStream.writeObject(message);
+
+		return "redirect:/";
+	}
+}
+
+class Message implements Serializable {
+	private String title;
+	private String content;
+
+	public Message() {
+
 	}
 
+	public Message(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Message{" +
+				"title='" + title + '\'' +
+				", content='" + content + '\'' +
+				'}';
+	}
 }
